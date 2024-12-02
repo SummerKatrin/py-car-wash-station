57689e49bd0763d81ec57ac073841afc693d3f0a
Kateryna <katerina.a.biruk@gmail.com>
2024-12-02 22:00:40 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -1,19 +1,20 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+    def __init__(self, comfort_class: int,
+                 clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int):
+    def __init__(self, distance_from_city_center: float, clean_power: int,
+                 average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-
-    def serve_cars(self, cars: list):
+    def serve_cars(self, cars: list) -> float:
         income = 0
         for car in cars:
             price = self.calculate_washing_price(car)
@@ -21,21 +22,19 @@ class CarWashStation:
                 income += price
         return income
 
-
-    def calculate_washing_price(self, car: Car):
-        power = car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating
+    def calculate_washing_price(self, car: Car) -> float:
+        power = (car.comfort_class * (self.clean_power - car.clean_mark)
+                 * self.average_rating)
         divisor = self.distance_from_city_center
         return round(power / divisor , 1)
 
-
-    def wash_single_car(self, car: Car):
+    def wash_single_car(self, car: Car) -> bool:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
             return True
         return False
 
-
-    def rate_service(self, rating: int):
+    def rate_service(self, rating: int) -> None:
         calculated_rating = self.average_rating * self.count_of_ratings
         new_rating = calculated_rating + rating
         self.count_of_ratings += 1
